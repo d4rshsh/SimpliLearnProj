e295a72494b1308292255227ec3518e49b7dc1b7
boots10 <100184967+d4rshsh@users.noreply.github.com>
2022-09-10 18:52:28 +0530

    Project 1

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplilearnProj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          string filepath = @"C:\MPHASIS LEARNING ACADEMY\sourceCode.txt";
+
+          List<string> lines = new List<string>();
+
+           lines =  File.ReadAllLines(filepath).ToList();
+
+            foreach (string item in lines)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
